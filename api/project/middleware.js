82bb0ed0d1465c9fa0d1588ecc762340d2c71caa
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-18 13:35:15 -0600

    WIP

--- api/project/middleware.js
+++ api/project/middleware.js
@@ -6,7 +6,11 @@ async function checkProjectNameUnique(req, res, next)
     {
         const project_name = req.body.project_name;
         const exists = await Project.findByProjectName(project_name);
-        if (exists)
+        
+        if (!req.body.project_name || !req.body.project_name.trim())
+        {
+            next({ status: 400, message: "Project_name required" });
+        } else if (exists)  
         {
             next({ status: 400, message: `Resource name ${req.body.project_name} already exists` });
         }
